2a408715f13b9583e54847c4823bd787024a26dd
Juliana Ferreira <juliana.ferreira@ericsson.com>
2024-04-22 20:06:37 -0300

    config database and created a new class

---
+++ API/Entities/Product.cs
@@ -0,0 +1,11 @@
+using Microsoft.Net.Http.Headers;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
